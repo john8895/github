eaad5f9c46f323d7f0024e2fb9f86374eeb8b812
JohnChen <paiching01@gmail.com>
2020-01-23 09:37:17 +0800

    case 20

--- comon.js
+++ comon.js
@@ -7,3 +7,19 @@
 function my$(id){
     return document.getElementById(id);
 }
+
+function setInnerText(element, text) {
+    if (typeof element.textContent == "undefined") {
+        element.innerText = text;
+    } else {
+        element.textContent = text;
+    }
+}
+
+function getInnerText(element){
+    if(typeof element.textContent == "undefined"){
+        return element.innerText;
+    }else {
+        return element.textContent;
+    }
+}
