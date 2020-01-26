024dffe538b9778fb381462e4584a446401e86d1
JohnChen <paiching01@gmail.com>
2020-01-26 08:55:03 +0800

    move files to directory

---
+++ 第二節/07-美女相冊/common.js
@@ -0,0 +1,25 @@
+
+/**
+ * 
+ * @param {*} id 
+ */
+
+function my$(id){
+    return document.getElementById(id);
+}
+
+function setInnerText(element, text) {
+    if (typeof element.textContent == "undefined") {
+        element.innerText = text;
+    } else {
+        element.textContent = text;
+    }
+}
+
+function getInnerText(element){
+    if(typeof element.textContent == "undefined"){
+        return element.innerText;
+    }else {
+        return element.textContent;
+    }
+}
