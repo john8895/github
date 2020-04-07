1a3f0bba7b284689aefe56bb64e19d2bcf763924
JohnChen <paiching01@gmail.com>
2020-04-08 06:45:35 +0800

    order meat system

---
+++ 0407.js
@@ -0,0 +1,4 @@
+javascript:void ((function (d) {
+    if (self != top || d.getElementById('toolbar') && d.getElementById('toolbar').getAttribute('data-resizer')) return false;
+    d.write('<!DOCTYPE HTML><html style="opacity:0;"><head><meta charset="utf-8"/></head><body><a data-viewport="320x480" data-icon="mobile">Mobile (e.g. Apple iPhone)</a><a data-viewport="320x568" data-icon="mobile" data-version="5">Apple iPhone 5</a><a data-viewport="375×667" data-icon="mobile" data-version="7">Apple iPhone 7</a><a data-viewport="414×736" data-icon="mobile" data-version="7+">Apple iPhone 7 Plus</a><a data-viewport="600x800" data-icon="small-tablet">Small Tablet</a><a data-viewport="768x1024" data-icon="tablet">Tablet (e.g. Apple iPad 2-3rd, mini)</a><a data-viewport="1280x800" data-icon="notebook">Widescreen</a><a data-viewport="1920×1080" data-icon="tv">HDTV 1080p</a><script src="https://lab.maltewassermann.com/viewport-resizer/resizer.min.js"></script></body></html>')
+})(document));
